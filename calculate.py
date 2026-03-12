3b078e2c0dedd8a5a61b1252c82115681b314354
zhekeens <zhekeens@MacBook-Air-Evgenij.local>
2026-03-12 14:39:16 +0700

    last push

--- calculate.py
+++ calculate.py
@@ -1,7 +1,8 @@
+import math
 while(True):
     answer = input("Введите ваше урованений:")
     result = 0
-    if('*' in answer):
+    if ('*' in answer):
         answer = answer.split('*')
         result = int(answer[0]) * int(answer[1])
     elif '-' in answer:
@@ -13,6 +14,19 @@ while(True):
     elif '+' in answer:
         answer = answer.split('+')
         result = int(answer[0]) + int(answer[1])
+    elif '**' in answer:
+        answer = answer.split('**')
+        result = int(answer[0]) ** int(answer[1])
+    elif '|' in answer:
+        answer = answer.split('|')
+        result = math.sqrt(int(answer[0]))
+    elif 'fac' in answer:
+        factorial = 1
+        number = answer.split('fac')
+
+        for i in range(2, int(number[1]) + 1):
+            factorial *= i
+            result = factorial
 
     print("Привет земляне")
     print(result)
